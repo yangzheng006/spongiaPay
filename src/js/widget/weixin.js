a014f40572ca01667548727d9cb566991decf488
wanming <wanming@tiaohuo.com>
2017-12-28 16:19:23 +0800

    使用积分

--- src/js/widget/weixin.js
+++ src/js/widget/weixin.js
@@ -321,7 +321,7 @@ var weixin = {
     weixin.init().done(function (config) {
       wx.ready(function () {
         wx.scanQRCode({
-          desc: '扫一扫',
+          desc: '请扫描商家收款码',
           needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
           scanType: ["qrCode", "barCode"] // 可以指定扫二维码还是一维码，默认二者都有
         });
